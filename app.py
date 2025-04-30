f67480e3d7b82618bb3b6b0fd75364588dee7ac7
Yash Vishnoi <yashvishnoi01.yv@gmail.com>
2025-04-30 12:55:56 +0530

    Changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request,jsonify
 import requests
+import os
 app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
@@ -33,4 +34,5 @@ def fetch_conversion_factor(source_currency,amount,destination_currency):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=True, host='0.0.0.0', port=port)
